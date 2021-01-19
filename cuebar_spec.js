d079361ccc2ef838821fe27fc0a53a1142fa5568
Dom <dominicson@gmail.com>
2021-01-19 10:59:45 -0800

    Update cuebar_spec.js
    Updated with ECMA-6 syntax.

--- cuebar_spec.js
+++ cuebar_spec.js
@@ -1,26 +1,23 @@
-describe("Que Bar Specs", function () {
+describe("Cue Bar Specs", () => {
 
-    describe("The Que Marker", function () {
-        it("offsets the position of the marker greater than the actual position because of the que bar's position ", function () {
+    describe("The Cue Marker", () => {
+        it("offsets the position of the marker greater than the actual position because of the Cue bar's position ", () => {
             expect(CueBar.mark(105, 7055)).toBeCloseTo('28.48');
             expect(CueBar.mark(20, 7055)).toBeCloseTo('20');
         });
 
-        it("moves the que bar toward the right of the que bar", function () {
+        it("moves the Cue bar toward the right of the Cue bar", () => {
+            let container = SpecDOM('#bar_marker');
             SpecDOM().html('<div id="bar_marker" style="left: 18px"/>');
-            var container = SpecDOM('#bar_marker');
             container.css("position", "absolute");
-
             CueBar.mark(105, 7055);
-
             expect(container.css("left")).toBeGreaterThan('20px');
             expect(container.css("left")).toBeLessThan('29px');
         });
     });
 
-    describe("The Que Bar", function () {
-
-        beforeEach(function () {
+    describe("The Cue Bar", () => {
+        beforeEach(() => {
             cues = [
                 { metadata: 'http://www.facebook.com/milyoni',
                     time: 246},
@@ -33,16 +30,16 @@ describe("Que Bar Specs", function () {
             ];
         });
 
-        it("returns the right kind of cue object", function () {
+        it("returns the right kind of cue object", () => {
             expect(CueBar.type(cues[0].metadata)).toEqual('like');
             expect(CueBar.type(cues[1].metadata)).toEqual('clip');
             expect(CueBar.type(cues[2].metadata)).toEqual('quote');
             expect(CueBar.type(cues[3].metadata)).toEqual('commentary');
         });
 
-        it("has placed an icon on the que bar", function () {
+        it("has placed an icon on the Cue bar", () => {
             SpecDOM().html('<div id="icon_bar"/></div>');
-            var container = SpecDOM('div#icon_bar');
+            let container = SpecDOM('div#icon_bar');
 
             container.css('position', 'relative');
             container.css('width', '704');
@@ -51,46 +48,41 @@ describe("Que Bar Specs", function () {
 
             expect($(container[0].children[0]).attr('onclick')).toMatch('milyoni.seek');
             expect($(container[0].children[0]).attr('src')).toMatch('_icon.png');
-
         });
 
-        it("shows the right popup partial"),function () {
-            var clip_commentary = { created_at:"2011-10-07T17:40:28Z",
+        it("shows the right popup partial"),() => {
+            let clip_commentary = { created_at:"2011-10-07T17:40:28Z",
                 id: 76,
                 is_commentary: true,
                 movie_id: 44,
                 name: "Steven Bauer: Scarface on Facebook",
-                thumbnail_url: "http://brightcove.vo.ll....jpg?pubId=937222930001",
+                thumbnail_url: "http://brightcove.vo.ll....jpg?pubId=1111",
                 updated_at: "2011-10-07T17:40:28Z",
                 video_id: "1161803744001"
             };
-            var quote = { created_at:"2011-08-08T20:59:44Z",
+            let quote = { created_at:"2011-08-08T20:59:44Z",
                 id: 15,
                 movie_id: 44,
                 quoted_at: null,
                 text: "are we in napa now?!",
                 updated_at: "2011-08-08T20:59:44Z"
             };
-            var clip_normal = { created_at:"2011-10-07T17:33:25Z",
+            let clip_normal = { created_at:"2011-10-07T17:33:25Z",
                 id:75,
                 is_commentary:null,
                 movie_id:44,
                 name:"I'm the Dude (R)",
-                thumbnail_url:"http://brightcove.vo.ll....jpg?pubId=937222930001",
+                thumbnail_url:"http://brightcove.vo.ll....jpg?pubId=1111",
                 updated_at:"2011-10-07T17:33:25Z",
                 video_id: "1079164021001"
             };
-            var like = { created_at: "2011-10-05T18:13:27Z",
+            let like = { created_at: "2011-10-05T18:13:27Z",
                 id: 38, link: "http://www.facebook.com/BigLebowskiMovie",
                 movie_id: 44,
                 name: "Like",
                 picture_url: "http://www.facebook.com/BigLebowskiMovie",
                 updated_at: "2011-10-05T18:13:27Z"
             };
-
-//            expect(CueBar.popupScreen(clip_commentary)).toEqual("mull");
         }
     });
-
-
 });
