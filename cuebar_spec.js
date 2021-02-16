c7e07ec60d543c90b640de0473ef25a9c4bb2094
Dom <dominicson@gmail.com>
2021-02-15 16:31:30 -0800

    Update cuebar_spec.js
    mark renamed to markAt

--- cuebar_spec.js
+++ cuebar_spec.js
@@ -1,16 +1,19 @@
 describe("Cue Bar Specs", () => {
 
-    describe("The Cue Marker", () => {
+    describe("markAt - The Cue's Marker", () => {
         it("offsets the position of the marker greater than the actual position because of the Cue bar's position ", () => {
-            expect(CueBar.mark(105, 7055)).toBeCloseTo('28.48');
-            expect(CueBar.mark(20, 7055)).toBeCloseTo('20');
+            expect(CueBar.markAt(105, 7055)).toBeCloseTo('28.48');
+            expect(CueBar.markAt(20, 7055)).toBeCloseTo('20');
         });
 
         it("moves the Cue bar toward the right of the Cue bar", () => {
             let container = SpecDOM('#bar_marker');
+     
             SpecDOM().html('<div id="bar_marker" style="left: 18px"/>');
             container.css("position", "absolute");
-            CueBar.mark(105, 7055);
+            
+            CueBar.markAt(105, 7055);
+            
             expect(container.css("left")).toBeGreaterThan('20px');
             expect(container.css("left")).toBeLessThan('29px');
         });
